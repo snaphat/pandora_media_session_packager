3dc4fdad71c300eaed6caba17bf59b1652105eab
Aaron Landwehr <snaphat@gmail.com>
2021-04-04 18:20:52 -0400

    Add icon.

--- make.ps1
+++ make.ps1
@@ -5,19 +5,19 @@ $file = "pandora_media_session.user.js"
 $directory = "$(Get-Location)/pandora_media_session"
 $script = "$directory/$file"
 $script = Get-Content $script
-$author      = ($script | Select-String -pattern "@author +([A-z0-9 ].*)").Matches.Groups[1]
+$author = ($script | Select-String -pattern "@author +([A-z0-9 ].*)").Matches.Groups[1]
 $description = ($script | Select-String -pattern "@description +([A-z0-9 ].*)").Matches.Groups[1]
-$name        = ($script | Select-String -pattern "@name +([A-z0-9 ].*)").Matches.Groups[1]
-$version     = ($script | Select-String -pattern "@version +([A-z0-9 ].*)").Matches.Groups[1]
+$name = ($script | Select-String -pattern "@name +([A-z0-9 ].*)").Matches.Groups[1]
+$version = ($script | Select-String -pattern "@version +([A-z0-9 ].*)").Matches.Groups[1]
 
 
-$manifest="{
+$manifest = "{
     `"author`": `"$author`",
     `"manifest_version`": 2,
     `"name`": `"$name`",
     `"version`": `"$version`",
     `"description`": `"$description`",
-    `"minimum_chrome_version`": `"88.0.0.0`",
+    `"minimum_chrome_version`": `"87.0.0.0`",
     `"content_scripts`": [
         {
             `"matches`": [`"*://*.pandora.com/*`"],
@@ -25,12 +25,20 @@ $manifest="{
             `"run_at`": `"document_start`"
         }
     ],
+    `"icons`": {
+        `"64`": `"assets/pandora_64x64.png`",
+        `"128`": `"assets/pandora_128x128.png`"
+    },
         `"browser_action`": {
-        `"default_title`": `"$name`"
+        `"default_title`": `"$name`",
+        `"default_icon`": `"assets/pandora_64x64.png`"
     },
         `"permissions`": [
         `"*://*.pandora.com/*`"
     ]
 }"
 $manifest | Out-File -Encoding ascii -FilePath "$directory/manifest.json"
+mkdir "$directory/assets"
+copy "./assets/pandora_64x64.png" "$directory/assets"
+copy "./assets/pandora_128x128.png" "$directory/assets"
 zip pandora_media_session.zip "$directory/*"
